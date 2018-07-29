a4a37fd87a3f5e6fe8cd57c6936ce80ec76bc018
kklee998 <kahkinlee998@gmail.com>
2018-07-29 08:12:22 +0800

    added 2 anomaly

--- app.py
+++ app.py
@@ -13,13 +13,26 @@ def start(bot,update):
 	bot.send_message(chat_id=update.message.chat_id,text="TGL2018 UST10 Chat bot! Press 1 to begin!")
 
 def graph(bot,update):
-	bot.send_photo(chat_id=update.message.chat_id, photo=open('gender.png', 'rb'))	
+	bot.send_photo(chat_id=update.message.chat_id, photo=open('gender.png', 'rb'))
+
+def anomaly222(bot,update):
+	bot.send_photo(chat_id=update.message.chat_id, photo =open('Card222.PNG', 'rb'))
+
+def anomaly880(bot,update):
+	bot.send_photo(chat_id=update.message.chat_id, photo=open('Card880.PNG','rb'))	
 
 
 
 start_handler = CommandHandler('start',start)
 dispatcher.add_handler(start_handler)
-graph_handler = CommandHandler('1',graph)
+
+graph_handler = CommandHandler('graph',graph)
 dispatcher.add_handler(graph_handler)
+
+anomaly222_handler = CommandHandler('222',anomaly222)
+dispatcher.add_handler(anomaly222_handler)
+
+anomaly880_handler = CommandHandler('880',anomaly880)
+dispatcher.add_handler(anomaly880_handler)
 
 updater.start_polling()
